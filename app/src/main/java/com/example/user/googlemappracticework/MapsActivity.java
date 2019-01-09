7c2177937ab9d22f908f0505f764ebad60d65ccf
User <usern1503@gmail.com>
2019-01-09 17:25:00 +0500

    new commit

--- app/src/main/java/com/example/user/googlemappracticework/MapsActivity.java
+++ app/src/main/java/com/example/user/googlemappracticework/MapsActivity.java
@@ -5,7 +5,10 @@ import android.content.pm.PackageManager;
 import android.graphics.Camera;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -26,18 +29,34 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.io.IOException;
 import java.util.List;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, View.OnClickListener {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
 
     private EditText etSearch;
 
+    private Location currentLocation;
+    private LocationManager locationManager;
+    private static final long MIN_TIME = 0;
+    private static final float MIN_DISTANCE = 100;
+
+    private Marker marker;
+
     View mapView;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference dbReferenceLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +68,11 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        //below VIEW is used for moving location button to specific position
         mapView = mapFragment.getView();
+
+        firebaseDatabase= FirebaseDatabase.getInstance();
+        dbReferenceLocation=firebaseDatabase.getReference("LOCATIONS");
 
         etSearch = findViewById(R.id.etSearch);
         etSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
@@ -64,14 +87,49 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         });
 
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Grant the Permission", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Toast.makeText(this, "GPS is disabled", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //after checking required permissions
+        //getCurrent location without  pressing any button
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
+
+    }
+
+    public void saveToFirebase(final Location location) {
+
+        String id= dbReferenceLocation.push().getKey();
+
+        dbReferenceLocation.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Gujranwala and move/animate the camera
-        LatLng latLng = new LatLng(32.073419, 74.210114);
+        // Add (first way) a marker in Gujranwala and move/animate the camera
+ /*      LatLng latLng = new LatLng(32.073419, 74.210114);
         MarkerOptions options = new MarkerOptions()
                 .title("This is title")
                 .position(latLng);
@@ -79,7 +137,10 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         //animate camera here
         CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latLng, 10);
-        mMap.animateCamera(update);
+        mMap.animateCamera(update);*/
+
+        // Add (second way) a marker in Gujranwala and move/animate the camera
+        marker = mMap.addMarker(new MarkerOptions().position(new LatLng(32, 74)).title("Title Here"));
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             Toast.makeText(this, "Grant the Permission", Toast.LENGTH_LONG).show();
@@ -100,14 +161,14 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         // position on right bottom
         rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
         rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
-        rlp.setMargins(0, 280, 100, 30);
+        rlp.setMargins(0, 250, 100, 30);
     }
 
     public void locationBtnClick() {
         mMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
             @Override
             public boolean onMyLocationButtonClick() {
-                LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+                //LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
                 if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
                     Toast.makeText(MapsActivity.this, "GPS is disabled.", Toast.LENGTH_LONG).show();
                     return true;
@@ -138,7 +199,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
             double lat = address.getLatitude();
             double lng = address.getLongitude();
-            goToLocationZoom(lat, lng, 10);
+            goToLocationZoom(lat, lng, 15);
 
         } catch (IOException e) {
             Toast.makeText(this, "in catch block", Toast.LENGTH_SHORT).show();
@@ -150,6 +211,41 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_map, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        //mMap.clear();
+        goToLocationZoom(location.getLatitude(), location.getLongitude(), 15);
+/*
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        MarkerOptions options = new MarkerOptions()
+                .title("This is my Location")
+                .position(latLng);
+        mMap.addMarker(options);
+*/
+
+        marker.remove();
+        marker = mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(location.getLatitude(), location.getLongitude()))
+                .title("My Location"));
+
+        saveToFirebase(location);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 
     @Override
@@ -175,11 +271,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             return super.onOptionsItemSelected(item);
         }
         return true;
-    }
-
-    @Override
-    public void onClick(View v) {
-
     }
 
 }
